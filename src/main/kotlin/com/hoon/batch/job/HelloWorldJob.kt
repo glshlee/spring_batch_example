614b2ba856f86775a838e603cf223c9f11825a36
sherlock.hoons <sherlock.hoons@kakaopaycorp.com>
2022-02-10 13:56:42 +0900

    hello world

---
+++ src/main/kotlin/com/hoon/batch/job/HelloWorldJob.kt
@@ -0,0 +1,33 @@
+package com.hoon.batch.job
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class HelloWorldJob(
+    private val jobBuilderFactory: JobBuilderFactory,
+    private val stepBuilderFactory: StepBuilderFactory
+) {
+
+    @Bean
+    fun step(): Step {
+        return stepBuilderFactory.get("step1")
+            .tasklet { _, _ ->
+                println("Hello, World!")
+                RepeatStatus.FINISHED
+            }
+            .build()
+    }
+
+    @Bean
+    fun job(step: Step): Job {
+        return jobBuilderFactory.get("job")
+            .start(step)
+            .build()
+    }
+}
---
+++ src/test/kotlin/com/hoon/batch/BatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hoon.batch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
